9ad23b2576069f7395a2cb65246403e661f5eab1
kolyjjj <me.koly.li@gmail.com>
2016-03-30 16:44:51 +0800

    sorting for string

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
 
 var bubble = function(array) {
-  var len = array.length;
+  var input = (typeof array === 'string' ? array.split('') : array);
+  var len = input.length;
   for (var i = 0; i < len - 1; i++) {
       for (var j = len - 1; j > i; j--) {
-          if (array[j] < array[j - 1]) {
-              var temp = array[j];
-              array[j] = array[j-1];
-              array[j-1] = temp;
+          if (input[j] < input[j - 1]) {
+              var temp = input[j];
+              input[j] = input[j-1];
+              input[j-1] = temp;
             }
         }
     }
 
-  return array;
+  return (typeof array === 'string') ? input.join('') : input;
 };
 
 var sort = {
--- test/index.spec.js
+++ test/index.spec.js
@@ -5,11 +5,16 @@ var sort = require('../index');
 
 describe('sorting', function(){
 
-  it('bubble sorting', function(){
+  it('bubble sort', function(){
     expect(sort.bubble([])).to.deep.equal([]);
     expect(sort.bubble([1])).to.deep.equal([1]);
     expect(sort.bubble([1, 2, 3, 4, 5])).to.deep.equal([1, 2, 3, 4, 5]);
     expect(sort.bubble([5, 4, 3, 2, 1])).to.deep.equal([1, 2, 3, 4, 5]);
     expect(sort.bubble([3,2,1,4,5,6,11])).to.deep.equal([1,2,3,4,5,6,11]);
+    expect(sort.bubble('one')).to.equal('eno');
   });
+
+  it('insertion sort', function(){
+    expect(sort.insertion(3,2,1,4,5,6,11)).to.deep.equal([1,2,3,4,5,6,11]);
+    });
 });
